3d25264a8e64bc2a14def724093a6a2fef46835c
Racy Daniel Godino <brracy@gmail.com>
2024-11-18 19:47:44 +0600

    Initial commit

---
+++ core/core/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+admin.site.site_header = "APON"
+admin.site.site_title = "APON Admin Portal"
+admin.site.index_title = "Welcome to APON Portal"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace='home'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
